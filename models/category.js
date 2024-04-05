1c393091c3fb039b491ca259e4dfdf5e1dd8868b
Braco <isovicramiz07@gmail.com>
2024-04-05 05:08:25 -0400

    remove aisle code

--- models/category.js
+++ models/category.js
@@ -5,13 +5,6 @@ const Schema = mongoose.Schema;
 const CategorySchema = new Schema({
   name: { type: String, required: true, maxLength: 100, minLength: 3 },
   description: { type: String, required: true, minLength: 3 },
-  // aisle: [
-  //   {
-  //     type: Schema.Types.ObjectId,
-  //     ref: "Aisle",
-  //     required: true,
-  //   },
-  // ],
 });
 
 CategorySchema.virtual("url").get(function () {
